8bd068e10fcae07c522201fdaefc5ff53a9109a2
Nathaniel Watts <reg@nathanielwatts.com>
2018-06-04 07:57:26 -0500

    Fix namespacing + ensure class dependencies met

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,18 +1,24 @@
 # https://www.tntware.com/donorhub/groups/developers/wiki/how-can-my-fundraising-app-use-the-donorhub-api.aspx
 # DONATIONS QUERY
 
-class Api::V1::DonationsController < V1Controller
-  def create
-    load_donations
-  end
+require_dependency "api/v1_controller"
 
-  protected
+module Api
+  module V1
+    class DonationsController < V1Controller
+      def create
+        load_donations
+      end
 
-  def load_donations
-    @donations ||= donation_scope.by_date_range(params[:date_from], params[:date_to])
-  end
+      protected
 
-  def donation_scope
-    current_designation_profile_or_member.donations.distinct
+      def load_donations
+        @donations ||= donation_scope.by_date_range(params[:date_from], params[:date_to])
+      end
+
+      def donation_scope
+        current_designation_profile_or_member.donations.distinct
+      end
+    end
   end
 end
