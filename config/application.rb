7f12f61c9e368097fe1b0aace708ce5665437ec8
Nathaniel Watts <reg@nathanielwatts.com>
2018-06-04 07:47:10 -0500

    Update Application Name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AucklandEvDonationServices
+module DonorhubTransformer
   class Application < Rails::Application
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
