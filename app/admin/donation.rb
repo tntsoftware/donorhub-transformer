369bd4b1a1e204b2fd590d005e35a86f5f10c424
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-02-21 10:32:26 +1300

    add donations view

---
+++ app/admin/donation.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Donation do
+  controller do
+    def scoped_collection
+      end_of_association_chain.joins(:designation_account).where(designation_accounts: { active: true })
+    end
+  end
+
+  filter :designation_account, collection: DesignationAccount.where(active: true)
+  filter :donation_account
+
+  index do
+    id_column
+    column :designation_account
+    column :donor_account
+    column(:amount) { |donation| number_to_currency(donation.amount, unit: "#{donation.currency} ") }
+    column :created_at
+    actions
+  end
+end
