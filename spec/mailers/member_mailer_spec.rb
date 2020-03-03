0dfdfde4de22b14405d73defb274905220c85012
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-03-03 16:51:22 +1300

    add mailer spec

---
+++ spec/mailers/member_mailer_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MemberMailer, type: :mailer do
+  let(:member) { create(:member) }
+
+  describe '#inform' do
+    subject!(:mail) { described_class.inform(member) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq "Sign into MPDX using your donation services credentials"
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([member.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([ENV['ORG_CONTACT_EMAIL']])
+    end
+  end
+end
