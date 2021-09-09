f71ec9739195f675af046f71608115ec55a27a21
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-09 22:44:15 +1200

    replace font with IBM Plex Sans

--- app/javascript/packs/modules/chartjs.js
+++ app/javascript/packs/modules/chartjs.js
@@ -1,6 +1,5 @@
 // Usage: https://www.chartjs.org/
 import Chart from "chart.js";
 
-Chart.defaults.global.defaultFontFamily =
-  "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+Chart.defaults.global.defaultFontFamily = "'IBM Plex Sans', sans-serif";
 window.Chart = Chart;
